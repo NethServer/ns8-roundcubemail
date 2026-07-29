439ad9837d553c986259cee3ba184ac2b3207eca
Andrea Leardini <andrea.leardini@nethesis.it>
2026-07-29 11:45:00 +0200

    fix(ui): ns8 module maintenance (#119)
    * chore(ui): update module default logo
    
    * build(ui): tune webpack chunk size and silence sass deprecations
    
    * chore(ui): disable no-console eslint rule
    
    * chore(ui): add format script to package.json
    
    * test(ui): import Browser inside the Take screenshots test
    
    * build: require core >= 3.20.1
    
    * build(ui): update axios
    
    * build(ui): update Vue to 2.7
    
    * build(ui): update ns8-ui-lib to latest 2.x
    
    * style(ui): format code with prettier

--- build-images.sh
+++ build-images.sh
@@ -54,7 +54,7 @@ buildah config --entrypoint=/ \
     --label="org.nethserver.authorizations=traefik@node:routeadm mail@any:mailadm cluster:accountconsumer" \
     --label="org.nethserver.tcp-ports-demand=1" \
     --label="org.nethserver.rootfull=0" \
-    --label="org.nethserver.min-core=3.12.4-0" \
+    --label="org.nethserver.min-core=3.20.1" \
     --label="org.nethserver.images=docker.io/mariadb:10.11.18 ${roundcube_image}" \
     "${container}"
 # Commit the image
